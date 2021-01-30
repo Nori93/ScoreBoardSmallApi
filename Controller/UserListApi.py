8f6a5bb47165f8844ac04de0957f5d39ea1a9638
Norbert <airdarkangel1@gmail.com>
2021-01-30 18:08:58 +0100

    f(develop): make UserListApi.

---
+++ Controller/UserListApi.py
@@ -0,0 +1,28 @@
+
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with 
+from Repository.dbConnector import User,db
+
+class UserListApi(Resource):
+   
+
+    scoreList_fields = {
+        'id': fields.Integer,
+        'score':fields.String,
+        'date':fields.DateTime,
+        'user_id':fields.Integer
+    }
+
+    userList_fields = {        
+            'id': fields.Integer,
+            'name': fields.String,
+            'clientGuid': fields.String,
+            'userGuid': fields.String,
+            'score_list': fields.List(fields.Nested(scoreList_fields))
+    }
+
+    @marshal_with(userList_fields)
+    def get(self):
+        result = User.query.options(db.lazyload('score_list')).all()
+        return result;
+
+   
--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from main import api,app
 from Controller.UserApi import UserApi
+from Controller.UserListApi import UserListApi
 from Controller.ScoreBoardApi import ScoreBoardApi
 
 api.add_resource(ScoreBoardApi,"/scoreboard/<string:userguid>")
-api.add_resource(UserApi,"/user")
+api.add_resource(UserListApi,"/user")
+api.add_resource(UserApi,"/user/<string:userguid>")
 if __name__ == '__main__':
     app.run(debug=True)
